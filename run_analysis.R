d54cde5cee02c332f996eb4dbd5a1f84ac88c893
lingani <malick.lingani@gmail.com>
2014-08-20 23:23:15 +0000

    run_analysis.R updated: column rename
    subjects renamed to sujectID and activityLabel renamed to activity

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 #settig working directory
 #set this directory to where your sumsung data is enziped
-#setwd("F:/Cours/Coursera/The Data Science Specialization/Module 03 - Getting and Cleaning Data/data/Assessments")
+setwd("F:/Cours/Coursera/The Data Science Specialization/Module 03 - Getting and Cleaning Data/data/Assessments")
 
 #loading required libraries
 library(plyr)
@@ -33,7 +33,7 @@ names(subject_test)<-"subjectID"
 
 #Merging the training and the test sets
 merged_train_and_test_set<-rbind(train_set, test_set)
-merged_train_and_test_set$subjects= c(subject_train$subjectID, subject_test$subjectID)
+merged_train_and_test_set$subjectID= c(subject_train$subjectID, subject_test$subjectID)
 merged_train_and_test_set$experiment= c(rep("Train", dim(train_set)[1]), rep("Test",dim(test_set)[1]))
 
 ########################################################################
@@ -49,7 +49,7 @@ selected_data<-merged_train_and_test_set[ ,selected_features$featuresID]
 names(selected_data)<-selected_features$featuresLabel
 
 #names(selected_data)<-selected_features$featuresLabel
-selected_data<-cbind( subjects=merged_train_and_test_set$subjects, experiment=merged_train_and_test_set$experiment, selected_data)
+selected_data<-cbind( subjectID=merged_train_and_test_set$subjectID, experiment=merged_train_and_test_set$experiment, selected_data)
 
 
 
@@ -68,7 +68,7 @@ train_labels<-read.table(train_labels_URL, sep=" ", header=FALSE)
 train_labels$order <- seq(len=nrow(train_labels))
 train_activity_labels<-merge(train_labels, activity_labels, by.x="V1", by.y="V1", all.x = TRUE, sort=FALSE)
 train_activity_labels<-train_activity_labels[sort.list(train_activity_labels$order), -2]
-names(train_activity_labels)<-c("activityID", "activityLabel")
+names(train_activity_labels)<-c("activityID", "activity")
 
 #Reading Test labels
 test_labels_URL<-paste0(dataDir,"/test/y_test.txt" )
@@ -77,13 +77,13 @@ test_labels<-read.table(test_labels_URL, sep=" ", header=FALSE)
 test_labels$order <- seq(len=nrow(test_labels))
 test_activity_labels<-merge(test_labels,activity_labels, by.x="V1", by.y="V1", all.x = TRUE, sort=FALSE)
 test_activity_labels<-test_activity_labels[sort.list(test_activity_labels$order), -2]
-names(test_activity_labels)<-c("activityID", "activityLabel")
+names(test_activity_labels)<-c("activityID", "activity")
 
 #Merging Train activity labels and test activity labels
 merged_activity_labels<-rbind(train_activity_labels, test_activity_labels)
 
 #naming the activities in the data set "selected_data"
-selected_data<-cbind(activityLabel=merged_activity_labels$activityLabel, selected_data)
+selected_data<-cbind(activity=merged_activity_labels$activity, selected_data)
 
 
 
@@ -102,7 +102,7 @@ names(selected_data)<-gsub("-std", "Std",names(selected_data))
 ##      of each variable for each activity and each subject. 
 ########################################################################
 data<-selected_data[ ,-3]
-tidy_data<-ddply(data, .(activityLabel, subjects), colwise(mean))
+tidy_data<-ddply(data, .(activity, subjectID), colwise(mean))
 tidy_data_URL<-paste0(dataDir,"/tidy_data.txt" )
 write.table(tidy_data, tidy_data_URL, row.name=FALSE)
 
